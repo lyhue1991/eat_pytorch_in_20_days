2cef275d4cb95047e54fbb806e0b270e2c401cfc
lyhue1991 <lyhue1991@163.com>
2020-06-27 19:00:23 +0800

    add Sponsor.md

--- push-to-github.py
+++ push-to-github.py
@@ -17,7 +17,7 @@
 
 # !git rm --cached  .ipynb_checkpoints/* 
 
-# !git commit -m "add 1-1" 
+# !git commit -m "add LICENSE" 
 
 # !git remote add origin https://github.com/lyhue1991/eat_pytorch_in_20_days
 
