e26d8c60e5b9b3fea0e093ef9cad234453475ba9
lyhue1991 <lyhue1991@163.com>
2021-07-04 11:04:53 +0800

    add houji

--- push-to-github.py
+++ push-to-github.py
@@ -17,7 +17,7 @@
 
 # !git rm --cached  .ipynb_checkpoints/* 
 
-# !git commit -m "add torchkeras.LightModel" 
+# !git commit -m "add houji" 
 
 # !git remote add origin https://github.com/lyhue1991/eat_pytorch_in_20_days
 
